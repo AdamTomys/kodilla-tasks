ef15e08aa527fef10f198845243d0313e2a660ec
Adam Tomys <adamtomys@gmail.com>
2020-11-25 18:36:52 +0100

    begin new trello task implementation

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -21,10 +21,12 @@ public class TrelloController {
 
         List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
 
-        trelloBoards.stream()
-                .filter(board -> board.getName() != null && board.getId() != null && board.getName().contains("Kodilla"))
-                .forEach(board -> {
-            System.out.println(board.getId() + " " + board.getName());
+        trelloBoards.forEach(trelloBoardDto -> {
+            System.out.println(trelloBoardDto.getId() + " - " + trelloBoardDto.getName());
+            System.out.println("This board contains lists: ");
+            trelloBoardDto.getLists().forEach(trelloList -> {
+                System.out.println(trelloList.getName() + " - " + trelloList.getId() + " - " + trelloList.isClosed());
+            });
         });
     }
 }
