3ad8f7d0d53eb1de1fbd144f569f0adbbe5e6d87
Adam Tomys <adamtomys@gmail.com>
2021-02-10 18:44:20 +0100

    thymeleaf math examples

--- src/test/java/com/crud/tasks/controller/TaskControllerTest.java
+++ src/test/java/com/crud/tasks/controller/TaskControllerTest.java
@@ -20,9 +20,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
 
 @SpringJUnitWebConfig
